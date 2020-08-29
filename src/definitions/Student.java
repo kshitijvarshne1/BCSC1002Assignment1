ed0fee4746f966468f3da34e28bcf4648417f112
Kshitij Varshney <kshitij.varshney_cs19@gla.ac.in>
2020-08-29 21:15:00 +0530

    add the constructor to Student.java

--- src/definitions/Student.java
+++ src/definitions/Student.java
@@ -12,4 +12,23 @@ public class Student {
     private long universityRollNumber;
     private int numberOfBooksIssuedByTheStudent;
     private Book[] namesOfTheBooksIssuedByTheStudent;
+
+    // Parameterized Constructor
+    public Student(String studentName, long universityRollNumber, int numberOfBooksIssuedByTheStudent, Book[] namesOfTheBooksIssuedByTheStudent) {
+        this.studentName = studentName;
+        this.universityRollNumber = universityRollNumber;
+        this.numberOfBooksIssuedByTheStudent = numberOfBooksIssuedByTheStudent;
+        this.namesOfTheBooksIssuedByTheStudent = namesOfTheBooksIssuedByTheStudent;
+    }
+
+    // Non-Parameterized Constructor
+    public Student() {
+        this.studentName = "";
+        this.universityRollNumber = 0L;
+        this.numberOfBooksIssuedByTheStudent = 0;
+        this.namesOfTheBooksIssuedByTheStudent = new Book[NUMBER_OF_MAXIMUM_BOOKS_ISSUE];
+        for (int index = 0; index < NUMBER_OF_MAXIMUM_BOOKS_ISSUE; index++) {
+            this.namesOfTheBooksIssuedByTheStudent[index] = new Book();
+        }
+    }
 }
