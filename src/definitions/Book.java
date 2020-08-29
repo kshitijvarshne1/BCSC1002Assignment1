dc3e6fc5aad86cc69ebfccd3284a64070b02908d
Kshitij Varshney <kshitij.varshney_cs19@gla.ac.in>
2020-08-29 19:28:47 +0530

    add the constructors

--- src/definitions/Book.java
+++ src/definitions/Book.java
@@ -10,4 +10,16 @@ public class Book {
     private String bookName;
     private String authorName;
     private String thirteenDigitISBNNumber;
+
+    public Book() {
+        this.bookName = null;
+        this.authorName = null;
+        this.thirteenDigitISBNNumber = null;
+    }
+
+    public Book(String bookName, String authorName, String thirteenDigitISBNNumber) {
+        this.bookName = bookName;
+        this.authorName = authorName;
+        this.thirteenDigitISBNNumber = thirteenDigitISBNNumber;
+    }
 }
