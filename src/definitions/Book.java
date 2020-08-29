f429701165a91d3e1190de5212b6fb64a81afcfe
Kshitij Varshney <kshitij.varshney_cs19@gla.ac.in>
2020-08-29 19:32:46 +0530

    add the getter and setter method

--- src/definitions/Book.java
+++ src/definitions/Book.java
@@ -22,4 +22,28 @@ public class Book {
         this.authorName = authorName;
         this.thirteenDigitISBNNumber = thirteenDigitISBNNumber;
     }
+
+    public String getBookName() {
+        return bookName;
+    }
+
+    public void setBookName(String bookName) {
+        this.bookName = bookName;
+    }
+
+    public String getAuthorName() {
+        return authorName;
+    }
+
+    public void setAuthorName(String authorName) {
+        this.authorName = authorName;
+    }
+
+    public String getThirteenDigitISBNNumber() {
+        return thirteenDigitISBNNumber;
+    }
+
+    public void setThirteenDigitISBNNumber(String thirteenDigitISBNNumber) {
+        this.thirteenDigitISBNNumber = thirteenDigitISBNNumber;
+    }
 }
