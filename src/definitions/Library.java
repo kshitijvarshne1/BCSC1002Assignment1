e160d60a13a054cccd243834208b3743d4a56263
Kshitij Varshney <kshitij.varshney_cs19@gla.ac.in>
2020-08-29 20:11:38 +0530

    add the equal() and hascode() method to the Library.java

--- src/definitions/Library.java
+++ src/definitions/Library.java
@@ -39,4 +39,17 @@ public class Library {
     public String toString() {
         return Arrays.toString(booksAvailableInLibrary);
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Library library = (Library) o;
+        return Arrays.equals(getBooksAvailableInLibrary(), library.getBooksAvailableInLibrary());
+    }
+
+    @Override
+    public int hashCode() {
+        return Arrays.hashCode(getBooksAvailableInLibrary());
+    }
 }
